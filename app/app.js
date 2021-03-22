54cebe67c915c62f4b929a4fbfbbba1b95451571
Josh Holloway <joshuaanthonyholloway@gmail.com>
2021-03-22 20:24:02 +0000

    cleaned up commenting in app.js

--- app/app.js
+++ app/app.js
@@ -12,32 +12,11 @@ app.use(express.static("static"));
 
 // Create end-points for transferring data between client and database
 
-// URL for sending requesting menu data from database
+// URL forrequesting menu data from database
 
 app.get("/menu-data", function(req, res){
 
-    // when we have the database here we would call a function to get the menu info from the DB
-    // The function to do this would be written in the data.js file, and then referenced here.
-    /* e.g function getMenuData(){
-        1. use SQL to get menu data from DB
-        2. convert that data into JSON to send back to the webpage
-        3. send data to webpage
-    
-
-    // Just for demonstration here is a JSON string on a couple of menu items which 
-    // I'll send to the webpage
-
-    var menu = {
-        item1: {id: "M001", name: "burger", price:5.99, description: "delicious burger with cheese"},
-        item2: {id: "M002", name: "pizza", price:5.99, description: "cheese and tomato pizza"},
-    }
-
-    res.json(menu);
-    }*/
-
-    // actual code, example left above for reference.
-
-    // call getMenu function to get menu data from db
+     // call getMenu function to get menu data from db
     dataFunctions.getMenu(function(menuItems){
         // send menuItems to client as JSON
         res.json(menuItems);
