0b214d275d0d7dbf8623c3e135cfb4de274c6c29
Josh Holloway <joshuaanthonyholloway@gmail.com>
2021-04-19 08:54:35 +0100

    add joshmenutest.js to populate menu in html file

---
+++ static/joshmenutest.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// get the main app
+var mainApp = angular.module("mainApp", []);
+
+// create the controller
+mainApp.controller("menuController", function($scope, $http){
+
+    // get request for menu data
+    $http.get('/menu-data').then(function(response){
+
+        $scope.items = response.data;
+    });
+});
