7146898e854333c279986b844313dd55f0bd771f
Josh Holloway <joshuaanthonyholloway@gmail.com>
2021-04-26 15:22:06 +0100

    add function to add items to basket

--- static/joshmenutest.js
+++ static/joshmenutest.js
@@ -12,8 +12,30 @@ mainApp.controller("menuController", function($scope, $http){
         $scope.items = response.data;
     });    
 
-    $scope.addToBasket = function(id){
+    $scope.addToBasket = function(id, name){
+        
         console.log(id);
+        // boolean value to record if the item being added is already in basket
+        var inBasketBoolean = 0;
+        console.log(inBasketBoolean);
+
+        // for loop to check if an item of same ID is already in basket
+        for (var item of $scope.basket){
+            if(item.ID == id){
+                item.Quantity = item.Quantity + 1
+
+                // update in basket boolean vlue to true
+                inBasketBoolean = 1;
+            };
+        };
+        console.log(inBasketBoolean);
+        // If statement to verify if item was already in basket
+        if (inBasketBoolean == 0){
+            console.log(name);
+            $scope.basket.push({ID: id, Name: name, Quantity: 1});
+        };
+
+        
     };
 
     $scope.basket = [];
