621a669a2ec9826184375d556bc3295b88ec2477
Josh Holloway <joshuaanthonyholloway@gmail.com>
2021-04-26 15:26:02 +0100

    added functionality to calculate price

--- static/joshmenutest.js
+++ static/joshmenutest.js
@@ -12,7 +12,7 @@ mainApp.controller("menuController", function($scope, $http){
         $scope.items = response.data;
     });    
 
-    $scope.addToBasket = function(id, name){
+    $scope.addToBasket = function(id, name, price){
         
         console.log(id);
         // boolean value to record if the item being added is already in basket
@@ -21,8 +21,14 @@ mainApp.controller("menuController", function($scope, $http){
 
         // for loop to check if an item of same ID is already in basket
         for (var item of $scope.basket){
+            
+            // If statement to check if id is already in basket
             if(item.ID == id){
+                // Increment quantity of item ordered
                 item.Quantity = item.Quantity + 1
+
+                // Update price for items ordered
+                item.Price = item.Price + price
 
                 // update in basket boolean vlue to true
                 inBasketBoolean = 1;
@@ -32,7 +38,7 @@ mainApp.controller("menuController", function($scope, $http){
         // If statement to verify if item was already in basket
         if (inBasketBoolean == 0){
             console.log(name);
-            $scope.basket.push({ID: id, Name: name, Quantity: 1});
+            $scope.basket.push({ID: id, Name: name, Quantity: 1, Price: price});
         };
 
         
